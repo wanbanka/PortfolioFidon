086f5d3fbd72190de2768423e787e10c0732102a
wanbanka <hurt.stress@gmail.com>
2020-06-19 17:19:35 +0200

    Avancement 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,58 @@
 import { Component } from '@angular/core';
 
+import {Location} from '@angular/common';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    
+    constructor(private location: Location){
+        this.nomPage = this.location.path().replace('/', '');
+        document.body.style.setProperty('--bg-page', this.rechercheCouleur().couleur);
+    }
   
     determine: boolean = false;
+nomPage: string;
+
+tableauCouleurs: any[] = [
+    {
+        page: '',
+     couleur: '#8ca0b9'
+    },
+    
+    {
+        page: 'presentation',
+        couleur: '#dcda6f'
+    },
+    
+    {
+        page: 'oeuvres',
+        couleur: '#f28e57'
+    },
+    
+    {
+        page: 'contact',
+        couleur: '#ffffff'
+    }
+];
 
 toggleMenu(){
     this.determine = !this.determine ? true : false;
+}
+
+rechercheCouleur(){
+    return this.tableauCouleurs.find((numero) => {
+        return this.nomPage === numero.page;
+    });
+}
+
+fermeMenu(page: string){
+    this.determine = false;
+    this.nomPage = page;
+    document.body.style.setProperty('--bg-page', this.rechercheCouleur().couleur);
 }
     
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -10,9 +10,15 @@ export class MenuComponent implements OnInit {
   constructor() { }
     
     @Input() open : boolean;
+
+    @Output() page = new EventEmitter<any>();
     
 
   ngOnInit() {
   }
+    
+    changePage(page: string){
+        this.page.emit(page);
+    }
 
 }
