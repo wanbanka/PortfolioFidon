acd61fa08c90e75982ac027164cae85bb56b3629
wanbanka <hurt.stress@gmail.com>
2020-07-20 17:33:26 +0200

    Avancement 20072020

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, QueryList } from '@angular/core';
 
 import {ColorsServiceService} from '../Services/colors-service.service';
 
@@ -13,7 +13,7 @@ declare const Konva: any;
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit {
 
   constructor(private colors: ColorsServiceService) { 
   
@@ -21,10 +21,10 @@ export class HomeComponent implements OnInit, AfterViewInit {
       
   }
     
-    index: number = 0;
-    
     largeurFenetre: number = window.innerWidth;
     hauteurFenetre: number = window.innerHeight;
+    
+    @ViewChild('layer', {static: false}) layer: KonvaComponent;
     
     @ViewChildren('rect') rects: QueryList<KonvaComponent>;
     
@@ -33,9 +33,9 @@ export class HomeComponent implements OnInit, AfterViewInit {
         height: this.hauteurFenetre
     });
 
-imagesRectangles: BehaviorSubject<any>[] = [];
+imagesRectangles: any[] = [];
 
-paths: string[] = ["assets/oeuvre.jpg", "assets/oeuvre2.jpg", "assets/oeuvre3.jpg", "assets/oeuvre4.jpg"];
+paths: string[] = ["assets/oeuvre.jpg", "assets/oeuvre2.jpg", "assets/oeuvre3.jpg", "assets/oeuvre4.jpg", "assets/oeuvre5.jpg", "assets/oeuvre6.jpg"];
     
     randomNumber(min: number, max: number): number{
         return Math.random() * (max - min) + min;
@@ -47,7 +47,7 @@ paths: string[] = ["assets/oeuvre.jpg", "assets/oeuvre2.jpg", "assets/oeuvre3.jp
            node: rectangle.getStage(),
             duration: 2,
             x: this.randomNumber(20, this.largeurFenetre - 200),
-             y: this.randomNumber(this.hauteurFenetre / 2, this.hauteurFenetre - 200),
+             y: this.randomNumber(this.hauteurFenetre / 4, this.hauteurFenetre - 200),
             opacity: 1,
             rotation: (Math.PI/180) * this.randomNumber(0, 360)
         });
@@ -63,30 +63,24 @@ paths: string[] = ["assets/oeuvre.jpg", "assets/oeuvre2.jpg", "assets/oeuvre3.jp
           
           let imagine = new Image();
           
-let configRect: BehaviorSubject<any> = new BehaviorSubject({
-   x: 220,
-    y: 150,
-    width: 0,
-    height: 0,
-    opacity: 0,
-    fillPatternImage: null
-});
+let configRect: any;
           
           imagine.src = path; 
           
           imagine.onload = () => {
-            configRect.next({
+            configRect = new BehaviorSubject({
    x: this.randomNumber(20, this.largeurFenetre - 200),
     y: this.hauteurFenetre + 10,
     width: imagine.width,
     height: imagine.height,
     opacity: 0,
+    draggable: true,
     fillPatternImage: imagine
 });
               
-              let vipRect = configRect;
+    
       
-      this.imagesRectangles = [...this.imagesRectangles, vipRect];
+      this.imagesRectangles = [...this.imagesRectangles, configRect];
               
               console.table(this.imagesRectangles);
               
@@ -99,9 +93,5 @@ let configRect: BehaviorSubject<any> = new BehaviorSubject({
       });
 
   }
-
-ngAfterViewInit(){
-    
-}
 
 }
