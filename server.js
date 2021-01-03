4d3ad7a09287ec2ee22902909a57a039b95d21b3
wanbanka <hurt.stress@gmail.com>
2021-01-03 14:29:12 +0100

    DEPLOY final

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
+const proxy = require('global-tunnel-ng');
 
 const app = express();
 
 app.use(express.static(__dirname + '/dist/PortfolioFidon'));
+proxy.initialize({
+    host: 'https://backofficefidon.frb.io',
+    port: 443,
+    proxyAuth: "jfidon:degre27if89pOjUc_p02"
+});
 
 app.get('/*', (req, res) => {
 
