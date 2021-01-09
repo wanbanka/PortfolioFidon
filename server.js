f93fa9c6fe7f01c7b06e8d82160155e23d749afa
wanbanka <hurt.stress@gmail.com>
2021-01-09 17:44:47 +0100

    UPDATE server.js

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
+const proxy = require('node-http-proxy');
 
 const app = express();
+
+proxy.createProxyServer({target: "https://backofficefidon.frb.io/"}).listen(8080);
+
 
 app.use(express.static(__dirname + '/dist/PortfolioFidon'));
 
 app.get('/*', (req, res) => {
 
+    res.writeHead(200, {'Content-Type': 'application/json'});
+
 res.sendFile(path.join(__dirname + '/dist/PortfolioFidon/index.html'));
+
+res.end();
 
 });
 
