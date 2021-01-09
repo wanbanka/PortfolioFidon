a085b1c28428376dcb0b290f93234e9dd341667b
wanbanka <hurt.stress@gmail.com>
2021-01-09 19:10:17 +0100

    UPDATE server.js

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const path = require('path');
-const proxy = require('node-http-proxy');
-const fs = require('fs');
+var globalTunnel = require('global-tunnel-ng');
 
 const app = express();
-
-proxy.createProxyServer({target: "https://backofficefidon.frb.io/", secure: true, ssl: {
-    key: fs.readFileSync('cacert.pem', 'utf8'),
-    cert: fs.readFileSync('cacert.pem', 'utf8')
-}}).listen(443);
 
 
 app.use(express.static(__dirname + '/dist/PortfolioFidon'));
 
 app.get('/*', (req, res) => {
 
+globalTunnel.initialize({
+    host: "https://backofficefidon.frb.io/",
+    port: 443
+});
 
 res.sendFile(path.join(__dirname + '/dist/PortfolioFidon/index.html'));
 
