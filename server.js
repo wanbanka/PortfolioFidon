9b04728ce67f19eb2878c57959fb2de79445887f
wanbanka <hurt.stress@gmail.com>
2021-01-10 11:58:33 +0100

    UPDATE htaccess

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-var globalTunnel = require('global-tunnel-ng');
 
 const app = express();
 
@@ -8,11 +7,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/PortfolioFidon'));
 
 app.get('/*', (req, res) => {
-
-globalTunnel.initialize({
-    host: "https://172.16.3.33/",
-    port: 443
-});
 
 res.sendFile(path.join(__dirname + '/dist/PortfolioFidon/index.html'));
 
