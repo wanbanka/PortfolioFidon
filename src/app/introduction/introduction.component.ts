08355c2a96105604fb71134d10c422efa04a70b5
wanbanka <hurt.stress@gmail.com>
2021-01-17 14:16:38 +0100

    UPDATE timeline

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -97,7 +97,10 @@ export class IntroductionComponent implements OnInit, OnDestroy {
         this.displayExhibitions = this.exhibitions.map((exhibition) => {
             return {
                 id: exhibition.id,
-                title: "" + exhibition.date_debut + " - " + exhibition.date_fin,
+                title: "" + exhibition.date_debut.getDate() + "/" + 
+                (exhibition.date_debut.getMonth() + 1) +"/" + exhibition.date_debut.getFullYear() + " - " + 
+                exhibition.date_fin.getDate() + "/" + 
+                (exhibition.date_fin.getMonth() + 1) +"/" + exhibition.date_fin.getFullYear(),
                 content: exhibition.description,
                 icon: null
             }
